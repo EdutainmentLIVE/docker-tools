316d8dbf195bb0b67e6797e614c1f76b933e51da
Taylor Fausak <taylor@fausak.me>
2020-10-30 09:24:44 -0400

    Combine build and install steps

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -8,10 +8,9 @@ cabal freeze
 cat cabal.project.freeze
 hash=$( checksum cabal.project.freeze )
 cache restore "cabal-$hash,cabal"
-cabal build
+cabal install --installdir output/bin --install-method copy apply-refact brittany hasktags hlint stan stylish-haskell weeder
 cache store "cabal-$hash" ~/.cabal/store
 mkdir --parents output/bin output/lib
-cabal install --installdir output/bin --install-method copy apply-refact brittany hasktags hlint stan stylish-haskell weeder
 strip output/bin/*
 cp --recursive --target-directory output/lib /opt/ghc/8.10.2/lib/ghc-8.10.2/llvm-passes /opt/ghc/8.10.2/lib/ghc-8.10.2/llvm-targets /opt/ghc/8.10.2/lib/ghc-8.10.2/package.conf.d /opt/ghc/8.10.2/lib/ghc-8.10.2/platformConstants /opt/ghc/8.10.2/lib/ghc-8.10.2/settings
 artifact push workflow output --expire-in 1w
