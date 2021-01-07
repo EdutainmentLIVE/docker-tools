46f3c37bef1155e5fd61b597117ef31fcf243701
Sara Lichtenstein <sara@itpro.tv>
2021-01-07 07:25:06 -0700

    [ch56705] Upgrade GHC

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -12,5 +12,5 @@ mkdir --parents output/bin output/lib
 cabal install --installdir output/bin --install-method copy apply-refact brittany hasktags hlint stan stylish-haskell weeder
 cache store "cabal-$hash" ~/.cabal/store
 strip output/bin/*
-cp --recursive --target-directory output/lib /opt/ghc/8.10.2/lib/ghc-8.10.2/llvm-passes /opt/ghc/8.10.2/lib/ghc-8.10.2/llvm-targets /opt/ghc/8.10.2/lib/ghc-8.10.2/package.conf.d /opt/ghc/8.10.2/lib/ghc-8.10.2/platformConstants /opt/ghc/8.10.2/lib/ghc-8.10.2/settings
+cp --recursive --target-directory output/lib /opt/ghc/8.10.3/lib/ghc-8.10.3/llvm-passes /opt/ghc/8.10.3/lib/ghc-8.10.3/llvm-targets /opt/ghc/8.10.3/lib/ghc-8.10.3/package.conf.d /opt/ghc/8.10.3/lib/ghc-8.10.3/platformConstants /opt/ghc/8.10.3/lib/ghc-8.10.3/settings
 artifact push workflow output --expire-in 1w
