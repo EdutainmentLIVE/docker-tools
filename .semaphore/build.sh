0d286fecbce58494cd90644073c70ba6f9029e09
Taylor Fausak <taylor@fausak.me>
2021-01-08 16:41:39 -0500

    Fix copy command

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -6,5 +6,5 @@ mkdir --parents .stack/root .stack/work output/bin output/lib
 ./stack.sh --local-bin-path output/bin build --copy-bins apply-refact brittany hasktags hlint stan stylish-haskell weeder
 cache store stack .stack
 strip output/bin/*
-./stack.sh exec -- sh -o xtrace -c 'target="$PWD/output/lib" cd "$STACK_ROOT/programs/x86_64-linux/ghc-tinfo6-8.10.3/lib/ghc-8.10.3" && cp --recursive --target-directory "$target" --verbose llvm-passes llvm-targets package.conf.d platformConstants settings'
+./stack.sh exec -- sh -o xtrace -c 'target="$PWD/output/lib" && cd "$STACK_ROOT/programs/x86_64-linux/ghc-tinfo6-8.10.3/lib/ghc-8.10.3" && cp --recursive --target-directory "$target" --verbose llvm-passes llvm-targets package.conf.d platformConstants settings'
 artifact push workflow output --expire-in 1w
