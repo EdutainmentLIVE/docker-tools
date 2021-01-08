8221fbb937a2466b1b9712c3c431272f4b23dacc
Taylor Fausak <taylor@fausak.me>
2021-01-08 12:09:52 -0500

    Try to fix build

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 checkout
 mkdir --parents output/bin output/lib
-docker run --interactive --rm --tty --volume "$PWD:$PWD" --workdir "$PWD" itprotv/stack:v2.5.1 stack --local-bin-path output/bin build --copy-bins apply-refact brittany hasktags hlint stan stylish-haskell weeder
+docker run --interactive --rm --tty --volume "$PWD:$PWD" --workdir "$PWD" itprotv/stack:v2.5.1 stack --color never --jobs 1 --local-bin-path output/bin --no-terminal build --copy-bins apply-refact brittany hasktags hlint stan stylish-haskell weeder
 strip output/bin/*
 d='/root/.stack/programs/x86_64-linux/ghc-tinfo6-8.10.3/lib/ghc-8.10.3'
 docker run --interactive --rm --tty --volume "$PWD:$PWD" --workdir "$PWD" itprotv/stack:v2.5.1 cp --recursive --target-directory output/lib --verbose "$d/llvm-passes" "$d/llvm-targets" "$d/package.conf.d" "$d/platformConstants" "$d/settings"
