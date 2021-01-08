0f8f45f4b02a2d9f66257e9fcf9106fe38b72acc
Taylor Fausak <taylor@fausak.me>
2021-01-08 11:18:11 -0500

    Switch to Stack
    Co-authored-by: Dustin <dustin@itpro.tv>
    Co-authored-by: Sara <sara@itpro.tv>

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -1,16 +1,9 @@
 #! /usr/bin/env sh
 set -o errexit
 checkout
-cache restore hackage
-cabal update
-cache store hackage ~/.cabal
-cabal freeze
-cat cabal.project.freeze
-hash=$( checksum cabal.project.freeze )
-cache restore "cabal-$hash,cabal"
 mkdir --parents output/bin output/lib
-cabal install --installdir output/bin --install-method copy apply-refact brittany hasktags hlint stan stylish-haskell weeder
-cache store "cabal-$hash" ~/.cabal/store
+docker run --interactive --rm --tty --volume "$PWD:$PWD" --workdir "$PWD" itprotv/stack:v2.5.1 stack --local-bin-path output/bin build --copy-bins apply-refact brittany hasktags hlint stan stylish-haskell weeder
 strip output/bin/*
-cp --recursive --target-directory output/lib /opt/ghc/8.10.3/lib/ghc-8.10.3/llvm-passes /opt/ghc/8.10.3/lib/ghc-8.10.3/llvm-targets /opt/ghc/8.10.3/lib/ghc-8.10.3/package.conf.d /opt/ghc/8.10.3/lib/ghc-8.10.3/platformConstants /opt/ghc/8.10.3/lib/ghc-8.10.3/settings
+d='/root/.stack/programs/x86_64-linux/ghc-tinfo6-8.10.3/lib/ghc-8.10.3'
+docker run --interactive --rm --tty --volume "$PWD:$PWD" --workdir "$PWD" itprotv/stack:v2.5.1 cp --recursive --target-directory output/lib --verbose "$d/llvm-passes" "$d/llvm-targets" "$d/package.conf.d" "$d/platformConstants" "$d/settings"
 artifact push workflow output --expire-in 1w
