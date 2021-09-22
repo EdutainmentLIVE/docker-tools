32660deab480eb312b431090e852c37746f6084d
Andres Schmois <andres@itpro.tv>
2021-09-22 14:06:40 -0400

    Fix semaphore path

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -6,5 +6,5 @@ mkdir --parents .stack/root .stack/work output/bin output/lib
 ./stack.sh --local-bin-path output/bin build --copy-bins apply-refact brittany hasktags hlint stan stylish-haskell weeder
 cache store stack .stack
 strip output/bin/*
-./stack.sh exec -- sh -c 'target="$PWD/output/lib" && cd "$STACK_ROOT/programs/x86_64-linux/ghc-tinfo6-8.10.4/lib/ghc-8.10.4" && cp --recursive --target-directory "$target" --verbose llvm-passes llvm-targets package.conf.d platformConstants settings'
+./stack.sh exec -- sh -c 'target="$PWD/output/lib" && cd "$STACK_ROOT/programs/x86_64-linux/ghc-tinfo6-8.10.7/lib/ghc-8.10.7" && cp --recursive --target-directory "$target" --verbose llvm-passes llvm-targets package.conf.d platformConstants settings'
 artifact push workflow output --expire-in 1w
