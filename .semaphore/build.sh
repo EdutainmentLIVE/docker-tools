b819fc2493173bd76a46287c82ad55ccfa7862cf
Taylor Fausak <taylor@fausak.me>
2022-05-23 08:32:27 -0400

    Try to build specific versions

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -3,7 +3,7 @@ set -o errexit
 checkout
 cache restore stack
 mkdir --parents .stack/root .stack/work output/bin output/lib
-./stack.sh --local-bin-path output/bin build --copy-bins apply-refact brittany hlint weeder
+./stack.sh --local-bin-path output/bin build --copy-bins apply-refact-0.9.3.0 brittany-0.14.0.2 hlint-3.3.6 weeder-2.3.1
 cache store stack .stack
 strip output/bin/*
 ./stack.sh exec -- sh -c 'target="$PWD/output/lib" && cd "$STACK_ROOT/programs/x86_64-linux/ghc-tinfo6-9.0.2/lib/ghc-9.0.2" && cp --recursive --target-directory "$target" --verbose llvm-passes llvm-targets package.conf.d platformConstants settings'
